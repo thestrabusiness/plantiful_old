9fc42d349dbe1c8a2a1d793d0b7d27513c24ba16
Anthony M <anthony.moffa@thoughtbot.com>
2019-07-03 17:18:25 -0400

    exposse elm front end at root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :plants, only: [:index, :new, :create, :show] do
     resources :waterings, only: [:create]
   end
-  root 'plants#index'
+  root 'application#index'
 end
