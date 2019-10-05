23cc04e04d54ed576f2a3132be05a78b24ad3b08
Anthony M <thestrabusiness@users.noreply.github.com>
2019-10-05 17:16:37 -0400

    Update placeholder url to point to master branch

--- app/elm/Plant.elm
+++ app/elm/Plant.elm
@@ -141,7 +141,7 @@ plantListDecoder =
 
 placeholderImage : String
 placeholderImage =
-    "https://raw.githubusercontent.com/thestrabusiness/plantiful/change-plant-list-styling/app/assets/images/default_plant.jpg"
+    "https://raw.githubusercontent.com/thestrabusiness/plantiful/master/app/assets/images/default_plant.jpg"
 
 
 plantDecoder : Decoder Plant
