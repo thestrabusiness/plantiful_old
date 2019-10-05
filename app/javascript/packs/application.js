6480c357d8e1381a6c2f30bd6eb0222255c61b4c
Anthony M <anthony.moffa@thoughtbot.com>
2019-10-05 11:44:46 -0400

    Fix undefined croppieTarget element when taking photo
    This commit fixes a bug when trying to add an avatar to a plant with a
    photo taken straight from the android camera while using android chrome.
    Upon returning to the page after taking a picture, the croppieTarget
    element would not be found and Croppie would not initialize. This commit
    adds some code to recursively check for the element until it appears,
    and then initialize Croppie.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,20 @@
 import { Elm } from '../../elm/Main';
 import Croppie from 'croppie/croppie';
 
+function rafAsync() {
+  return new Promise(resolve => {
+    requestAnimationFrame(resolve); //faster than set time out
+  });
+}
+
+function checkElement(selector) {
+  if (document.querySelector(selector) === null) {
+    return rafAsync().then(() => checkElement(selector));
+  } else {
+    return Promise.resolve(true);
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const csrfToken = document.getElementsByName('csrf-token')[0].content
   const target = document.createElement('div')
@@ -9,20 +23,21 @@ document.addEventListener('DOMContentLoaded', () => {
     node: target, flags: { csrfToken },
   })
 
-  app.ports.initJsCropper.subscribe(function(data){
-    const croppieButton = document.getElementById('croppie_button')
-    const croppieTarget = document.getElementById('croppie')
-    const opts = {
-      url: data,
-      viewport: { height: 300, width: 300 },
-      boundary: { height: 400, width: 400 },
-    }
-    let croppie = new Croppie(croppieTarget, opts )
-    croppieButton.addEventListener("click", function(){
-      croppie.result({format: 'jpeg', type: 'base64'}).then(function(result){
-        console.log(result)
-        app.ports.sendCroppedImage.send(result)
+  app.ports.initJsCropper.subscribe(async function(data){
+    checkElement('#croppie').then(result =>{
+      const croppieTarget = document.getElementById('croppie')
+      const croppieButton = document.getElementById('croppie_button')
+      const opts = {
+        url: data,
+        viewport: { height: 300, width: 300 },
+        boundary: { height: 400, width: 400 },
+      }
+      let croppie = new Croppie(croppieTarget, opts )
+      croppieButton.addEventListener("click", function(){
+        croppie.result({format: 'jpeg', type: 'base64'}).then(function(result){
+          app.ports.sendCroppedImage.send(result)
+        })
       })
-    })
+    });
   });
 })
